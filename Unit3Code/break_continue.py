f9693275b8656e18f280d819df08931eb0120428
smithbwlhs <smithb@wlwv.k12.or.us>
2023-10-10 09:45:01 -0700

    Create break_continue.py

---
+++ Unit3Code/break_continue.py
@@ -0,0 +1,35 @@
+'''
+Name: Brandon Smith
+Date: 10/9/23
+File name: break_continue.py
+Description: Shows how to use break and continue in Python
+'''
+
+'''while True:
+    user_num = int(input("Enter a postive number: "))
+    if user_num > 0:
+        break
+    print("Error: must be positive")
+print("Out of loop")
+
+while True:
+    user_num2 = int(input("Enter a positive number: "))
+    if user_num2 < 0:
+        print("Error: must be positive")
+        continue
+    else:
+        break'''
+
+'''for i in range(15):
+    print(i)
+    if i == 7:
+        break'''
+
+#printing 0-9on a single line
+print('\t',end='')
+for number in range(9):
+    print(number,end = '\n\t')
+print('')
+print("number \t power")
+
+ 
