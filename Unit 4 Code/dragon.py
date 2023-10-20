7c65adcd8bf0dfeaf46495a970c0aef953b2fbbe
smithbwlhs <smithb@wlwv.k12.or.us>
2023-10-20 14:50:05 -0700

    new files

---
+++ Unit 4 Code/dragon.py
@@ -0,0 +1,92 @@
+import random
+import time
+
+random.seed(7815)
+
+'''
+Name: Brandon Smith
+Date: 10/20/23
+File name: dragon.py
+Description: A text based game where an explorer chooses
+what cave to enter and deals with the consequences
+'''
+
+def display_intro():
+    """Prints an introduction to the game
+    
+    Parameters:
+    -----------
+    None
+
+    Returns:
+    --------
+    None
+    """
+    print()
+    print('''You are in a land full of dragons. In front of you,
+you see two caves. In one cave, the dragon is friendly
+and will share his treasure with you. The other dragon
+is greedy and hungry and will eat you on sight.''')
+    print()
+    
+
+def choose_cave():
+    """Asks a user to choose the cave they want to enter. It will
+    prompt them until they enter either 1 or 2.
+    
+    Parameters:
+    -----------
+    None
+
+    Returns:
+    --------
+    cave: string
+        The cave the user chose
+    """
+    cave = ''
+    while cave != '1' and cave != '2':
+        print("Which cave will you go into? (1 or 2)")
+        cave = input()
+    return cave
+
+def check_cave(chosen_cave):
+    """Determines if the user is eaten or not based on
+    their cave choice.
+    
+    Parameters
+    ----------
+    chosen_cave: str
+        The cave the user chose
+
+    Returns
+    -------
+    None
+    """
+    print("You approach the cave...")
+    time.sleep(2)
+    print("It's dark and spooky...")
+    time.sleep(2)
+    print('''A large dragon jumps out in front of you!
+He opens his jaws and...''')
+    print()
+    time.sleep(2)
+
+    friendly_cave = random.randint(1,2)
+    if chosen_cave == str(friendly_cave):
+        print("Give you his treasure!")
+    else:
+        print("Gobbles you down in one bite!")
+
+
+
+####Function calls####
+
+play_again = "yes"
+
+while play_again.lower() == "yes":
+    display_intro()
+    user_cave = choose_cave()
+    check_cave(user_cave)
+
+    print("Do you want to play agian? (yes or no)")
+    play_again = input()
--- Unit3Code/nested_loops.py
+++ Unit3Code/nested_loops.py
@@ -33,4 +33,15 @@ while num < 6:
         print("*",end = ' ')
     num = num + 1
     print('')
-    
+
+#make a 5x5 multiplication table
+# 1 2  3  4  5
+# 2 4  6  8  10
+# 3 6  9  12 15
+# 4 8  12 16 20
+# 5 10 15 20 25
+
+for row in range(1,6):
+    for column in range(1,6):
+        print(row * column, end = '\t')
+    print('')    
