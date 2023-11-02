7432387c919b270277644cbf6a19182ee0f43b54
smithbwlhs <smithb@wlwv.k12.or.us>
2023-11-02 10:04:16 -0700

    U4L4

---
+++ Unit 4 Code/Unit4L4/Unit4L4.py
@@ -0,0 +1,26 @@
+'''Unit 4 Lesson 4
+Review of functions, scope, and learn about
+return values'''
+
+# A function that initializes a variable
+
+
+# A function that accesses a variable outside its scope
+
+
+# A function that accesses more than one variable
+# outside its scope
+
+
+
+# A function that tries to modify a variable defined
+# outside its scope
+
+
+
+# Even/odd function
+
+
+####Main Code####
+v = 1
+x = 2
