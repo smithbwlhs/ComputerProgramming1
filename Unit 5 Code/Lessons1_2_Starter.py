f9d2c4d23c44703005390dc9ae25bd5ef505f310
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2023-11-27 13:51:34 -0800

    Create Lessons1_2_Starter.py

---
+++ Unit 5 Code/Lessons1_2_Starter.py
@@ -0,0 +1,38 @@
+# f strings in Python
+
+# String functions (upper, lower, strip, capitalize )
+ 
+# Accessing single characters within a string
+### M  i  L  L  i  e  ####
+### 0  1  2  3  4  5  ####
+### -6 -5 -4 -3 -2 -1 #### 
+
+
+
+# Number of occurences of a character in a string
+
+
+# Accessing multiple characters in a string(slicing)
+# syntax possiblities:
+# string[start:stop], string[start:], string[:stop]
+# string[:], string[start:stop:step], string[::step]
+# up to but not including the last index
+
+my_string = "This is a string"
+### T  h  i  s   i  s   a   s  t  r  i  n  g ###
+### 0  1  2  3 4 5  6 7 8 9 10 11 12 13 14 15
+
+# string[start:stop], string[start:], string[:stop]
+
+
+
+
+
+### T  h  i  s   i  s   a   s  t  r  i  n  g ###
+### 0  1  2  3 4 5  6 7 8 9 10 11 12 13 14 15
+
+# string[:], string[start:stop:step], string[::step]
+
+
+#backward string
+
